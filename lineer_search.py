02b593966c5b17d3b9c6449bb8928310a05c8486
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-05-29 09:25:27 +0300

    lineer_search

---
+++ lineer_search.py
@@ -0,0 +1,13 @@
+def lineer_arama(dizi, hedef):
+    n = len(dizi)
+
+    for i in range(n):
+        if dizi[i] == hedef:
+            return i
+        
+    return -1
+
+dizi = [1,2,4,5,8,10]
+hedef = 5
+
+print(lineer_arama(dizi,hedef))
